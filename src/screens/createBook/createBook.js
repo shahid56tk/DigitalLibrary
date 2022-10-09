baeaf7712f3d599ae135e6c2688c556df2ae5f0b
Shahid Hussain <shahid56tk@hotmail.com>
2022-10-09 22:18:02 +0500

    document picker installed

--- src/screens/createBook/createBook.js
+++ src/screens/createBook/createBook.js
@@ -1,12 +1,33 @@
-import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import app from '../../../api/firebase';
+import {getStorage, ref} from 'firebase/storage';
+import * as DocumentPicker from 'expo-document-picker';
+
+
+const storage = getStorage()
+
 
 
 const CreateBook = (props) =>{
+    const [file, setFile] = useState(null)
+
+    const getFile = async () => {
+        const res = await DocumentPicker.getDocumentAsync();
+        if (res.type === "success") {
+          console.log(res);
+          setFile(res.name)
+        } else {
+          console.log(res.type);
+        }
+      };
     return(
         <View style = {styles.container}>
-            <Text> Hello</Text>
+            <Button
+                title = {'Choose File'}
+                onPress= {() => getFile()}
+            />
+            <Text>{ file }</Text>
         </View>
     );
 };
