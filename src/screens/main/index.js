5bfd486ed41b0393129d3a9e1a1e76f160d6d011
Shahid Hussain <shahid56tk@hotmail.com>
2022-10-15 07:04:20 +0500

    book Category screen created

---
+++ src/screens/main/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, Text, StyleSheet, Button, FlatList, } from "react-native";
+import { Styles } from "../loginPage/style";
+let keys = ['a', 'b', 'c', 'd','e']
+
+const CategoryList = (props) =>{
+    return(
+        <View style= {Styles.container}>
+            <Text style = {Styles.mainText}>Please select catergory of you interest </Text>
+            <FlatList
+                data={keys}
+                renderItem= {({item})=>{
+                    return(
+                        <View style = {Styles.listItem}>
+                            <Text style = {Styles.itemTxt}> {keys} </Text>
+                        </View>
+                    )
+                }}
+            >
+
+            </FlatList>
+
+        </View>
+    )
+}
+
+const Styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    mainText:{
+        textAlign:'center',
+        fontSize:20,
+        backgroundColor:'gray',
+        width:'100%',
+        height:25,
+        borderBottomWidth:1,
+    },
+    listItem:{
+        width:'85%',
+        alignSelf:'center',
+        height:40,
+        backgroundColor:'green',
+        elevation:5,
+    },
+    itemTxt:{
+        textAlign:'center',
+        fontSize:18,
+    }
+
+
+})
